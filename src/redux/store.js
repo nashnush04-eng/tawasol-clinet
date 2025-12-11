6a3c831ca1a93205756c20e8b14f7b84c7f1657c
mostafa nashnush <nashnush@example.com>
2025-12-11 12:50:42 +0000

    Fix redux-thunk import and build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 import rootReducer from "./modules";
 import { setAuthToken } from "../utils";
 
 const initialState = {};
 
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
 
 const store = createStore(
   rootReducer,
